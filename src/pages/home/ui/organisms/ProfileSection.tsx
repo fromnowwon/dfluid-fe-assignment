5b633c6c75ced473d7eef4b7df561fd87c5500e4
Chaewon Jeong <fromnowwon@gmail.com>
2025-04-20 23:40:40 +0900

    feat: 프로필 순서 임의 배치 기능 추가

--- src/pages/home/ui/organisms/ProfileSection.tsx
+++ src/pages/home/ui/organisms/ProfileSection.tsx
@@ -1,8 +1,22 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import SectionTitle from '@/pages/home/ui/atoms/SectionTitle';
 import ProfileCard from '../molecules/ProfileCard';
-import { PROFILES_DATA } from '@/shared/data/profiles';
+import { Profile, PROFILES_DATA } from '@/shared/data/profiles';
+
+function shuffleArray<T>(array: T[]): T[] {
+  return [...array].sort(() => Math.random() - 0.5);
+}
 
 export default function ProfileSection() {
+  const [shuffledProfiles, setShuffledProfiles] = useState<Profile[]>([]);
+
+  useEffect(() => {
+    setShuffledProfiles(shuffleArray(PROFILES_DATA));
+  }, []);
+
   return (
     <section className="px-20 pt-30 pb-27.5">
       <SectionTitle>
@@ -12,7 +26,7 @@ export default function ProfileSection() {
       </SectionTitle>
 
       <div className="flex justify-center gap-5 mt-17.5">
-        {PROFILES_DATA.map((profile) => (
+        {shuffledProfiles.map((profile) => (
           <ProfileCard key={profile.id} {...profile} />
         ))}
       </div>
